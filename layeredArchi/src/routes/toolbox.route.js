6d803e08f270380fc5f6992be1d5eacabfdbaa94
abhishek sajwan <abhisheksajwan2004@gmail.com>
2025-11-24 16:04:19 +0530

    removed env

--- layeredArchi/src/routes/toolbox.route.js
+++ layeredArchi/src/routes/toolbox.route.js
@@ -6,8 +6,8 @@ import { ToolRepository } from "../repositories/ToolRepository.js";
 const router = express.Router();
 const toolRepository = new ToolRepository();
 const toolService = new ToolService(toolRepository);
-
 const toolController = new ToolController(toolService);
+
 
 // localhost:5000/api/tools/
 router.get("/", toolController.getAllTools);
@@ -16,16 +16,17 @@ router.get("/popular", toolController.getPopularTools);
 // localhost:5000/api/tools/category/frontend
 router.get("/category/:category", toolController.getToolsByCategory);
 
+// api/tools/:id
 router.get("/:id", toolController.getToolById);
-
+//api/tools/create
 router.post("/create", toolController.createTool);
-
+//api/tools/create/bulk
 router.post("/create/bulk", toolController.createBulkTools);
-
+//api/tools/:id
 router.put("/:id", toolController.updateTool);
-
+//api/tools/delete/bulk
 router.delete("/delete/bulk", toolController.deleteBulkTools);
-
+//api/tools/delete/:id
 router.delete("/delete/:id", toolController.deleteTool);
 
 export default router;
